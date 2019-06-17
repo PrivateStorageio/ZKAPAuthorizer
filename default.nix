aa9fab48ca6395e4c16e458cf2e1514bee495d53
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2019-06-17 13:00:00 -0400

    A more coherent build/test environment

--- default.nix
+++ default.nix
@@ -1,2 +1,14 @@
 { pkgs ? import <nixpkgs> { } }:
-pkgs.python37Packages.callPackage ./secure-access-token-authorizer.nix { }
+let
+  eliot = pkgs.pythonPackages.callPackage ./eliot.nix { };
+
+  # tahoe-lafs in nixpkgs is packaged as an application!  so we have to
+  # re-package it ourselves as a library.
+  tahoe-lafs = pkgs.pythonPackages.callPackage ./tahoe-lafs.nix {
+    inherit eliot;
+  };
+
+in
+  pkgs.pythonPackages.callPackage ./secure-access-token-authorizer.nix {
+    inherit tahoe-lafs;
+  }
