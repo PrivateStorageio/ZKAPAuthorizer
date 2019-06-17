dac9a434ac79c48b971f0689981b59cbd494146a
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2019-06-17 13:52:48 -0400

    A shell I can run the tests in
    don't like the duplication but not sure how to avoid it

--- shell.nix
+++ shell.nix
@@ -1,7 +1,13 @@
 { pkgs ? import <nixpkgs> { } }:
-(pkgs.python27.buildEnv.override {
-  extraLibs = [
-    (pkgs.callPackage ./default.nix { })
-  ];
-  ignoreCollisions = true;
-}).env
+let
+  satauthorizer = pkgs.callPackage ./default.nix { };
+in
+  (pkgs.python27.buildEnv.override {
+    extraLibs = [
+      pkgs.python27Packages.fixtures
+      pkgs.python27Packages.testtools
+      pkgs.python27Packages.hypothesis
+      satauthorizer
+    ];
+    ignoreCollisions = true;
+  }).env
