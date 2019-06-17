a1b3df5d4187ffc32f86055b44a22d76b2ee5ece
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2019-06-17 13:00:27 -0400

    The first semi-real test

--- src/_secureaccesstokenauthorizer/__init__.py
+++ src/_secureaccesstokenauthorizer/__init__.py
@@ -12,12 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+__all__ = [
+    "__version__",
+]
+
 __version__ = "0.0"
-
-from ._storage_server import (
-    SecureAccessTokenAuthorizerStorageServer,
-)
-
-from ._plugin import (
-    SecureAccessTokenAuthorizer,
-)
---
+++ src/_secureaccesstokenauthorizer/tests/test_plugin.py
@@ -0,0 +1,48 @@
+# Copyright 2019 PrivateStorage.io, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Tests for the Tahoe-LAFS plugin.
+"""
+
+from testtools import (
+    TestCase,
+)
+from testtools.matchers import (
+    Contains,
+)
+
+from allmydata.interfaces import (
+    IFoolscapStoragePlugin,
+)
+
+from twisted.plugin import (
+    getPlugins,
+)
+from twisted.plugins.secureaccesstokenauthorizer import (
+    storage_server,
+)
+
+class PluginTests(TestCase):
+    """
+    Tests for ``twisted.plugins.secureaccesstokenauthorizer.storage_server``.
+    """
+    def test_discoverable(self):
+        """
+        The plugin can be discovered.
+        """
+        self.assertThat(
+            getPlugins(IFoolscapStoragePlugin),
+            Contains(storage_server),
+        )
