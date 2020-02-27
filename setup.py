bf5ea05ed4e332747aa4a2fd6a2111c321178a13
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2020-02-27 09:42:10 -0500

    Define the package version using versioneer

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from setuptools import setup
 
+import versioneer
+
 setup(
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     package_data={
         "": ["testing-signing.key"],
     },
