a0ddec078b96d38058afb72bca79ef6827bc0ed6
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2020-05-07 20:18:21 -0400

    Try enabling coverage parallel feature so combine works better
    We're abusing combine to rewrite paths in the coverage data.  combine isn't
    intended to be used with only one coverage data file named ".coverage".  So
    enable parallel mode so we have only one coverage data file with the naming
    scheme "combine" expects.
    
    Maybe this gets us working path rewriting on Windows,

--- .circleci/report-coverage.sh
+++ .circleci/report-coverage.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i bash -p "python.withPackages (ps: [ ps.codecov ])"
-cp ./result-doc/share/doc/*/.coverage ./
-python -m coverage combine .coverage
+cp ./result-doc/share/doc/*/.coverage.* ./
+python -m coverage combine
 python -m coverage report
 python -m coverage xml
 codecov --file coverage.xml
