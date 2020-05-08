c9f6c0b8f83619b04b851b724b5f4661222d41da
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2020-05-07 20:31:00 -0400

    dump output paths to help debug failure to copy coverage out of nix build

--- .circleci/report-coverage.sh
+++ .circleci/report-coverage.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i bash -p "python.withPackages (ps: [ ps.codecov ])"
+find ./result-doc/share/doc
 cp ./result-doc/share/doc/*/.coverage.* ./
 python -m coverage combine
 python -m coverage report
