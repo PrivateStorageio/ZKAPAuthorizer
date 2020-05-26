11140273b285137413a960c180b08f86c03ef195
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2020-05-26 09:59:05 -0400

    Switch to the "bash uploader" as recommended by codecov support
    Currently builds are failing to upload coverage reports because:
    
    ```
    ==> Uploading
        .url https://codecov.io
        .query service=circleci&package=py2.0.15&job=1057.0&yaml=codecov.yml&token=<secret>&build=1057.0&branch=154.save-and-respond.1&commit=714379bfb318ec39566729b2fecce6339ff55daf&slug=PrivateStorageio%2FZKAPAuthorizer
        Pinging Codecov...
        Uploading to S3...
    Error: 400 Client Error: Bad Request for url: https://storage.googleapis.com/codecov/v4/raw/2020-05-26/3F10B6B6FEA878CCC9F58EAF87855DA8/714379bfb318ec39566729b2fecce6339ff55daf/17397def-dfec-47bb-b20f-879e223b0e90.txt?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=GOOG1EQX6OZVJGHKK3633AAFGLBUCOOATRACRQRQF6HMSMLYUP6EAD6XSWAAY%2F20200526%2FUS%2Fs3%2Faws4_request&X-Amz-Date=20200526T132703Z&X-Amz-Expires=10&X-Amz-SignedHeaders=host&X-Amz-Signature=86369f603a1c27782899d7df6206b35a4564c70fe4efaea3cb72dd03cfb80b36
    
    Tip: See all example repositories: https://github.com/codecov?query=example
    Support channels:
      Email:   hello@codecov.io
      IRC:     #codecov
      Gitter:  https://gitter.im/codecov/support
      Twitter: @codecov
    ```

--- .circleci/report-coverage.sh
+++ .circleci/report-coverage.sh
@@ -1,9 +1,11 @@
-#! /usr/bin/env nix-shell
-#! nix-shell -i bash -p "python.withPackages (ps: [ ps.codecov ])"
+#! /usr/bin/env bash
 set -x
 find ./result-doc/share/doc
 cp ./result-doc/share/doc/*/.coverage.* ./
 python -m coverage combine
 python -m coverage report
 python -m coverage xml
-codecov --file coverage.xml
+
+# Unfortunately, this is the recommended uploader.
+# https://docs.codecov.io/docs/about-the-codecov-bash-uploader
+bash <(curl -s https://codecov.io/bash)
