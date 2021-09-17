10be73149349daca80941c27a0145000acb89871
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2021-09-17 14:45:52 -0400

    Allow the Nix package to be built against Tahoe 1.16.0rc1

--- default.nix
+++ default.nix
@@ -3,12 +3,17 @@
 , collectCoverage ? false
 , testSuite ? null
 , trialArgs ? null
-, tahoe-lafs ? null
+, tahoe-lafs ? "1.14.0"
 }:
 let
+  tahoe-packages = {
+    "1.14.0"    = pkgs.python2Packages.tahoe-lafs-1_14;
+    "1.16.0rc1" = pkgs.python2Packages.callPackage ./nix/tahoe-lafs-1_16.nix { };
+  };
+  tahoe-lafs' = builtins.getAttr tahoe-lafs tahoe-packages;
+
   pkgs' = pkgs.extend (import ./overlays.nix);
   callPackage = pkgs'.python27Packages.callPackage;
-  tahoe-lafs' = if tahoe-lafs == null then pkgs.python2Packages.tahoe-lafs-1_14 else tahoe-lafs;
 in
 callPackage ./zkapauthorizer.nix {
   challenge-bypass-ristretto = callPackage ./python-challenge-bypass-ristretto.nix { };
