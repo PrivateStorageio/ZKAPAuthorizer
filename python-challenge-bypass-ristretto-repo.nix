e227af1742992882a5af2fd452a9db9efb30e1ec
Tom Prince <tom.prince@private.storage>
2021-10-14 13:23:32 -0600

    Remove old nix config.

--- python-challenge-bypass-ristretto-repo.nix
+++
@@ -1,9 +0,0 @@
-let
-  pkgs = import <nixpkgs> {};
-in
-  pkgs.fetchFromGitHub {
-    owner = "LeastAuthority";
-    repo = "python-challenge-bypass-ristretto";
-    rev = "f1a7cfab1a7f1bf8b3345c228c2183064889ad83";
-    sha256 = "12myak2jwaisljs7bmx1vydgd0fnxvkaisk4zsf0kshwxrlnyh3x";
-  }
