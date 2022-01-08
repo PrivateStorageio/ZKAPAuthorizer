5d2670ea4c0524efe39b86ae1517c5573d100242
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2022-01-08 08:24:08 -0500

    more general reasoning about special cases for provider choices

--- default.nix
+++ default.nix
@@ -41,16 +41,12 @@ in
       #   this another way, dependencies have undetected dependencies, easier
       #   to just use the wheel.
       collections-extended = "wheel";
-      # same as collections-extended
       isort = "wheel";
 
-      # From nixpkgs or sdist, fails with
-      # cp: cannot stat 'benchmark/': No such file or directory
-      # cp: cannot stat 'tests/': No such file or directory
+      # The sdists for these packages have a different source/directory layout
+      # than the github archive the nixpkgs derivations expects to operate on
+      # so the sdists provider fails to build them.
       tomli = "wheel";
-
-      # repo re-org or something?
-      # find: ‘hypothesis-6.32.1/hypothesis-python’: No such file or directory
       hypothesis = "wheel";
     };
   in
