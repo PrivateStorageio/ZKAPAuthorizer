3d87dfe9d1045b1f2d5f591e7be85e5baa9eb8ef
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2022-03-03 17:06:41 -0500

    use the ULP-distance-based matcher to match recovered floats
    In this commit the distance is constrained to 0 which is the same as exact
    value comparison.  I want to see what happens on other platforms with this
    configuration.  In a subsequent commit I will probably raise it to 4 which
    is the largest distance found by Rick Regan and written up on
    <https://www.exploringbinary.com/incorrect-decimal-to-floating-point-conversion-in-sqlite/>.
    He makes no guarantee that this is the greatest possible error but it seems
    like a reasonable next test.

--- src/_zkapauthorizer/tests/test_recover.py
+++ src/_zkapauthorizer/tests/test_recover.py
@@ -4,10 +4,10 @@ Tests for ``_zkapauthorizer.recover``, the replication recovery system.
 
 from asyncio import run
 from sqlite3 import Connection, connect
-from sys import float_info
-from typing import Dict, Iterator
+from typing import Dict, Iterator, Tuple, Union
 
 from allmydata.client import read_config
+from attrs import define, field
 from fixtures import TempDir
 from hypothesis import assume, given, note, settings
 from hypothesis.stateful import (
@@ -23,6 +23,7 @@ from testtools import TestCase
 from testtools.matchers import (
     AfterPreprocessing,
     Always,
+    Annotate,
     Equals,
     IsInstance,
     MatchesStructure,
@@ -48,7 +49,7 @@ from ..recover import (
 )
 from ..tahoe import MemoryGrid, Tahoe, link, make_directory, upload
 from .fixtures import Treq
-from .matchers import matches_capability
+from .matchers import matches_capability, matches_float_within_distance
 from .resources import client_manager
 from .sql import Table, create_table, escape
 from .strategies import (
@@ -70,19 +71,39 @@ def snapshot(connection: Connection) -> Iterator[str]:
         yield statement + "\n"
 
 
+SQLType = Union[int, float, str, bytes, None]
+
+
 def equals_db(reference: Connection):
     """
     :return: A matcher for a SQLite3 connection to a database with the same
         state as the reference connection's database.
     """
 
-    def structured_dump(db):
+    # The implementation strategy here is motivated by the need to apply a
+    # custom floating point comparison function.  This means we can't just
+    # compare dumped SQL statement strings.  Instead of trying to parse the
+    # SQL statement strings to extract the floating point values, we dump the
+    # database ourselves without bothering to generate the SQL statement
+    # strings in the first place.  Then we can dig into the resulting values,
+    # notice floats, and compare them with our custom logic.
+    #
+    # We need custom logic to compare floats because SQLite3 bugs cause
+    # certain values not to round-trip through the database correctly.  This
+    # is a huge bummer!  Fortunately the error is small and does not
+    # accumulate.
+
+    def structured_dump(db: Connection) -> Iterator[Tuple]:
+        """
+        Dump the whole database, schema and rows, without trying to do any string
+        formatting.
+        """
         tables = list(structured_dump_tables(db))
         for (name, sql) in tables:
             yield sql
             yield from structured_dump_table(db, name)
 
-    def structured_dump_tables(db):
+    def structured_dump_tables(db: Connection) -> Iterator[Tuple[str, str]]:
         curs = db.cursor()
         curs.execute(
             """
@@ -94,7 +115,13 @@ def equals_db(reference: Connection):
         )
         yield from iter(curs)
 
-    def structured_dump_table(db, table_name):
+    def structured_dump_table(
+        db: Connection, table_name: str
+    ) -> Iterator[Tuple[str, str, Tuple[SQLType, ...]]]:
+        """
+        Dump a single database table's rows without trying to do any string
+        formatting.
+        """
         curs = db.cursor()
         curs.execute(f"PRAGMA table_info({escape(table_name)})")
 
@@ -115,51 +142,70 @@ def equals_db(reference: Connection):
 
     return AfterPreprocessing(
         lambda actual: list(structured_dump(actual)),
-        _EqualsEnough(list(structured_dump(reference))),
+        _MatchesDump(list(structured_dump(reference))),
     )
 
 
-class _EqualsEnough:
-    def __init__(self, reference):
-        self.reference = reference
+@define
+class _MatchStatement:
+    """
+    Match a single structured SQL statement.  Statements are tuples like those
+    that ``equals_db`` deals with, not actual SQL strings.
+    """
+
+    reference = field()
 
     def match(self, actual):
-        def compare_row(n, actual, reference):
-            if actual[:1] == ("INSERT",):
-                actual_name, actual_row = actual[1:]
-                reference_name, reference_row = reference[1:]
-                if actual_name != reference_name:
-                    return Mismatch(
-                        "Row {} table name {} != {}".format(
-                            n, actual_name, reference_name
-                        )
-                    )
-                if len(actual_row) != len(reference_row):
-                    return Mismatch(
-                        "Row {} length {} != {}".format(
-                            n, len(actual_row), len(reference_row)
-                        )
-                    )
-                for (actual_field, reference_field) in zip(actual_row, reference_row):
-                    if isinstance(actual_field, float):
-                        if abs(actual_field - reference_field) > (
-                            10 * float_info.epsilon
-                        ):
-                            return Mismatch(
-                                "Row {} float {} too far from reference {}".format(
-                                    n, actual_field, reference_field
-                                )
-                            )
-            else:
-                if actual != reference:
-                    return Mismatch(
-                        "Row {} fields {} != {}".format(n, actual, reference)
-                    )
+        def match_field(reference):
+            if not isinstance(reference, float):
+                return Equals(reference)
 
+            # We can't compare floats for exact equality, not for the usual
+            # reason but because of limitations of SQLite3's support for
+            # floats.  This is particularly bad on Windows.
+            #
+            # https://www.exploringbinary.com/incorrect-decimal-to-floating-point-conversion-in-sqlite/
+            # https://www.mail-archive.com/sqlite-users@mailinglists.sqlite.org/msg56817.html
+            # https://www.sqlite.org/src/tktview?name=1248e6cda8
+            return matches_float_within_distance(reference, 0)
+
+        if actual[:1] == ("INSERT",):
+            # Match an insert-type statement.
+            actual_name, actual_row = actual[1:]
+            reference_name, reference_row = self.reference[1:]
+            if actual_name != reference_name:
+                return Mismatch(
+                    f"table name {actual_name} != {reference_name}",
+                )
+            if len(actual_row) != len(reference_row):
+                return Mismatch(
+                    f"length {len(actual_row)} != {len(reference_row)}",
+                )
+            for (actual_field, reference_field) in zip(actual_row, reference_row):
+                matcher = match_field(reference_field)
+                mismatch = matcher.match(actual_field)
+                if mismatch is not None:
+                    return mismatch
+        else:
+            # Match a DDL statement
+            return Equals(self.reference).match(actual)
+
+
+@define
+class _MatchesDump:
+    """
+    Match a complete database dump's worth of structured SQL statements.
+    """
+
+    reference = field()
+
+    def match(self, actual):
         for n, (a, r) in enumerate(zip(actual, self.reference)):
-            mismatch = compare_row(n, a, r)
+            mismatch = Annotate(f"row {n}", _MatchStatement(r)).match(a)
             if mismatch is not None:
-                return mismatch
+                return
+
+        return None
 
 
 class SnapshotMachine(RuleBasedStateMachine):
