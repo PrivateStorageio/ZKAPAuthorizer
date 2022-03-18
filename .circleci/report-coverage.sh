17459d5292cbea6fb99ce3becbe1bb5792d24782
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2022-03-18 13:58:50 -0400

    pull it out of the subdirectory

--- .circleci/report-coverage.sh
+++ .circleci/report-coverage.sh
@@ -2,7 +2,7 @@
 #! nix-shell -i bash -p "curl" -p "python.withPackages (ps: [ ps.coverage ])"
 set -x
 find ./result*/
-cp ./result*/coverage ./.coverage
+cp ./result*/coverage/.coverage ./.coverage
 python -m coverage xml
 
 # Unfortunately, this is the recommended uploader.
