21d31db69d90998ce1fb13c1166dc1ad4e98dccb
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2022-03-28 11:06:33 -0400

    boilerplate and a module docstring

--- src/_zkapauthorizer/replicate.py
+++ src/_zkapauthorizer/replicate.py
@@ -1,3 +1,27 @@
+# Copyright 2019 PrivateStorage.io, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+A system for replicating local SQLite3 database state to remote storage.
+"""
+
+__all__ = [
+    "ReplicationAlreadySetup",
+    "fail_setup_replication",
+    "setup_tahoe_lafs_replication",
+]
+
 from collections.abc import Awaitable
 
 from twisted.python.lockfile import FilesystemLock
