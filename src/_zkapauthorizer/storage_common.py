a399490caeb2c393ce4226e610b267fd8a659285
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2022-04-05 17:18:53 -0400

    type annotations for `MorePassesRequired`

--- src/_zkapauthorizer/storage_common.py
+++ src/_zkapauthorizer/storage_common.py
@@ -33,19 +33,18 @@ class MorePassesRequired(Exception):
     Storage operations fail with ``MorePassesRequired`` when they are not
     accompanied by a sufficient number of valid passes.
 
-    :ivar int valid_count: The number of valid passes presented in the
-        operation.
+    :ivar valid_count: The number of valid passes presented in the operation.
 
-    ivar int required_count: The number of valid passes which must be
-        presented for the operation to be authorized.
+    ivar required_count: The number of valid passes which must be presented
+        for the operation to be authorized.
 
-    :ivar set[int] signature_check_failed: Indices into the supplied list of
-        passes indicating passes which failed the signature check.
+    :ivar signature_check_failed: Indices into the supplied list of passes
+        indicating passes which failed the signature check.
     """
 
-    valid_count = attr.ib(validator=attr.validators.instance_of(int))
-    required_count = attr.ib(validator=attr.validators.instance_of(int))
-    signature_check_failed = attr.ib(converter=frozenset)
+    valid_count: int = attr.ib(validator=attr.validators.instance_of(int))
+    required_count: int = attr.ib(validator=attr.validators.instance_of(int))
+    signature_check_failed: frozenset[int] = attr.ib(converter=frozenset)
 
 
 def _message_maker(label: str) -> Callable[[str], bytes]:
