c6686ea082d5c71fc660768aa6dbfb8e5d38d019
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2022-04-27 12:26:07 -0400

    report to coveralls from the circleci jobs too

--- .circleci/report-coverage.sh
+++ .circleci/report-coverage.sh
@@ -1,10 +1,6 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i bash -p "curl" -p "python.withPackages (ps: [ ps.coverage ])"
+#! nix-shell -i bash -p "curl" -p "python.withPackages (ps: [ ps.coveralls ])"
 set -x
 find ./result*/
 cp ./result*/coverage/.coverage ./.coverage
-python -m coverage xml
-
-# Unfortunately, this is the recommended uploader.
-# https://docs.codecov.io/docs/about-the-codecov-bash-uploader
-bash <(curl -s https://codecov.io/bash)
+coveralls
