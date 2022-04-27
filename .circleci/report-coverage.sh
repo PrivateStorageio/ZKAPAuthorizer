2e43b8f8e08e70a086ae1733cee5cbfd7712915e
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2022-04-27 13:53:08 -0400

    give coveralls pyyaml so it can read the config file

--- .circleci/report-coverage.sh
+++ .circleci/report-coverage.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i bash -p "curl" -p "python3.withPackages (ps: [ ps.coveralls ])"
+#! nix-shell -i bash -p "curl" -p "python3.withPackages (ps: [ ps.coveralls ps.pyyaml ])"
 set -x
 find ./result*/
 cp ./result*/coverage/.coverage ./.coverage
