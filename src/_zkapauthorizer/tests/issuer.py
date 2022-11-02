922e909a4baa7dc31879c3951d6714be81f352c4
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2022-11-02 18:15:23 -0400

    Some docstrings/comments

--- src/_zkapauthorizer/tests/issuer.py
+++ src/_zkapauthorizer/tests/issuer.py
@@ -1,3 +1,7 @@
+"""
+A ZKAP issuer implemented as a L{twisted.web.resource.Resource}.
+"""
+
 from typing import Any
 
 from attrs import define
@@ -21,14 +25,35 @@ from .._types import ClientConfig, ServerConfig
 
 @define
 class Issuer:
+    """
+    Represent a running HTTP server which implements the ZKAP issuer API.
+
+    :ivar port: The listening network port object.
+
+    :ivar signing_key_path: The filesystem path to the file containing the
+        Ristretto signing key used by the running server.
+    """
+
     port: IListeningPort
     signing_key_path: FilePath
 
-    # testresources jams crap onto us
+    # Some application code uses values of type `Issuer` as "testresources"
+    # resources values.  Unfortunately after the "resource manager" creates
+    # the "resource" it sets attributes on the "resource" (one attribute for
+    # each dependency of the resource).  Issuer has no need of these as it has
+    # already been completely initialized by `__init__`.
+    #
+    # Define an extra attribute to swallow this value.  Unfortunately this
+    # also means the type can't be frozen. :/ But other than this we will try
+    # to behave as though it is frozen.
     issuer_dir: Any = None
 
     @property
     def allowed_public_keys(self) -> list[PublicKey]:
+        """
+        The public keys corresponding to the signing keys this server will
+        use.
+        """
         return [
             PublicKey.from_signing_key(
                 SigningKey.decode_base64(self.signing_key_path.getContent())
@@ -37,18 +62,30 @@ class Issuer:
 
     @property
     def encoded_allowed_public_keys(self) -> str:
+        """
+        The allowed public keys, serialized correctly to be written to a
+        Tahoe-LAFS configuration file.
+        """
         return ",".join(
             k.encode_base64().decode("ascii") for k in self.allowed_public_keys
         )
 
     @property
     def root_url(self) -> str:
+        """
+        The root resource for the ZKAP issuer HTTP API run by this server.
+        """
         address = self.port.getHost()
         assert isinstance(address, IPv4Address)
         return f"http://127.0.0.1:{address.port}/"
 
     @property
     def server_config(self) -> ServerConfig:
+        """
+        The configuration items to add to the ZKAPAuthorizer server
+        configuration section of the Tahoe-LAFS configuration file for a
+        ZKAP-enabled storage server.
+        """
         return {
             "ristretto-issuer-root-url": self.root_url,
             "ristretto-signing-key-path": self.signing_key_path.asTextMode().path,
@@ -56,6 +93,11 @@ class Issuer:
 
     @property
     def client_config(self) -> ClientConfig:
+        """
+        The configuration items to add to the ZKAPAuthorizer client
+        configuration section of the Tahoe-LAFS configuration file for a
+        ZKAP-enabled storage client.
+        """
         return {
             "redeemer": "ristretto",
             "ristretto-issuer-root-url": self.root_url,
@@ -64,6 +106,10 @@ class Issuer:
 
 
 class Redeem(Resource):
+    """
+    Implement the voucher redemption endpoint.
+    """
+
     def __init__(self, signing_key: SigningKey) -> None:
         Resource.__init__(self)
         self.signing_key = signing_key
