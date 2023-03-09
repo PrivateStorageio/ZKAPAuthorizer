41932f9a5a9dec929835c9af335e400230f04b01
Florian Sesser <florian@private.storage>
2023-03-09 19:27:41 +0000

    ... next step: Use our twisted package and see if it fixes the tests.

--- nix/twisted.nix
+++ nix/twisted.nix
@@ -1,7 +1,6 @@
 {twisted, fetchPypi, lib}:
 twisted.overrideAttrs (old: {
     src = fetchPypi {
-      #      inherit (old) pname;
       pname = "Twisted";
       version = "22.10.0";
       sha256 = "sha256-Mqy9QKlPX0bntCwQm/riswIlCUVWF4Oot6BZBI8tTTE=";
