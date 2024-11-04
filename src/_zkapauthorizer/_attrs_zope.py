29cdb61076b6b816c9efe078efe2de50aa14f4e8
Florian Sesser <florian@leastauthority.com>
2024-11-04 17:26:32 +0000

    Copy _attrs_zope.py type annotations from Klein
    This _attrs_zope.py has added type annotations, I found
    it in klein: https://github.com/twisted/klein/commit/bfd2592ded170a5946c7e34ba36b7d3290f77231
    Thanks, @glyph!

--- src/_zkapauthorizer/_attrs_zope.py
+++ src/_zkapauthorizer/_attrs_zope.py
@@ -1,17 +1,24 @@
-# type: ignore
+from __future__ import annotations
+
 import attrs
+from zope.interface import Interface
 
 
 @attrs.define(repr=False)
 class _ProvidesValidator:
-    interface = attrs.field()
+    interface: type[Interface] = attrs.field()
 
-    def __call__(self, inst, attr, value):
+    def __call__(
+        self, inst: object, attr: attrs.Attribute, value: object
+    ) -> None:
         """
         We use a callable class to be able to change the ``__repr__``.
         """
         if not self.interface.providedBy(value):
-            msg = f"'{attr.name}' must provide {self.interface!r} which {value!r} doesn't."
+            msg = (
+                f"'{attr.name}' must provide {self.interface!r} "
+                f"which {value!r} doesn't."
+            )
             raise TypeError(
                 msg,
                 attr,
@@ -19,11 +26,11 @@ class _ProvidesValidator:
                 value,
             )
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"<provides validator for interface {self.interface!r}>"
 
 
-def provides(interface):
+def provides(interface: type[Interface]) -> _ProvidesValidator:
     """
     A validator that raises a `TypeError` if the initializer is called
     with an object that does not provide the requested *interface* (checks are
