aae9a87beca799dc689578df0e2fc27a059e7dcd
Florian Sesser <florian@leastauthority.com>
2024-11-05 17:49:45 +0000

    Update pycddl 0.4.0 to 0.6.3

--- nix/pycddl.nix
+++ nix/pycddl.nix
@@ -1,12 +1,12 @@
 { lib, fetchPypi, buildPythonPackage, rustPlatform }:
 buildPythonPackage rec {
   pname = "pycddl";
-  version = "0.4.0";
+  version = "0.6.3";
   format = "pyproject";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-w0CGbPeiXyS74HqZXyiXhvaAMUaIj5onwjl9gWKAjqY=";
+    sha256 = "sha256-lVybSr+QvyepdTZfiTjqU0ENu6TT87ZZXIECBA8nMV4=";
   };
 
   nativeBuildInputs = with rustPlatform; [
@@ -17,6 +17,6 @@ buildPythonPackage rec {
   cargoDeps = rustPlatform.fetchCargoTarball {
     inherit src;
     name = "${pname}-${version}";
-    hash = "sha256-g96eeaqN9taPED4u+UKUcoitf5aTGFrW2/TOHoHEVHs=";
+    hash = "sha256-VpJ/PLAwwuakwsNAtLDdWGXCxl6jGMTvsEhzIHk6a0g=";
   };
 }
